4c425b8c7a213068c4d15a5b6ca8b7d7141dfb0f
Santi Liwsakphaiboon <oofpokemon@gmail.com>
2023-09-05 14:56:36 +0700

    feat: pages redirect

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,11 +1,17 @@
 'use client';
 import * as React from 'react';
+import { redirect } from 'next/navigation';
 import { AuthContext } from '@/core/types/authentication';
 import { AuthMachineContext } from '@/core/presentation/auth/authMachine';
 
 export default function Home() {
   const [state, send, service] = React.useContext<AuthContext>(AuthMachineContext);
   console.log('state: ', state);
+  if(state.matches('loggedOut')) {
+    redirect('/signin');
+  } else if (state.matches('loggedIn')) {
+    redirect('/dashboard');
+  }
   return (
     <main className="flex justify-center items-center w-full h-screen">
         {`${state.toStrings()}`}
